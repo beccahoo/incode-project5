3cafd4fb6dd1e791126fb7c3108ab7167a9456df
beccahoo <beccahoo@gmail.com>
2021-09-20 12:00:49 +1000

    created skeleton for sql database and update for package.json

--- index.js
+++ index.js
@@ -1,2 +1,10 @@
+require('dotenv').config();
 
-const express = require (express)
+const express = require('express');
+// Routes
+const homeRouter = require('./routes/home');
+const registerRouter = require('./routes/register');
+const loginRouter = require('./routes/login');
+const logoutRouter = require('./routes/logout');
+const schedulesRouter = require('./routes/schedules');
+const recoverPasswordRouter = require('./routes/recover-password');
