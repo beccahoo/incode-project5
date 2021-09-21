babd0952d3fba4f9bbe35837c030ca86bf040258
Vei Yie <veiyie1998@gmail.com>
2021-09-21 17:20:30 +0800

    Updated the validate email and password in the loginjs route file

--- routes/login.js
+++ routes/login.js
@@ -2,8 +2,24 @@ const express = require('express')
 const router = express.Router
 const bcrypt = require("bcryptjs")
 const db = require('../database')
+const { check, validationResult } = require('express-validator')
 
 router.post('/', (req, res) => {
+    const {email, password} = req.body
+
+    // 1. Validate email 
+    [ check( "email", "Please enter valid email")
+    .isEmail()
+    .normalizeEmail()
+    .trim()
+    .toLowerCase(),
+
+    check ("password", "Please enter valid password")
+    .isLength({min: 8})
+    .trim()
+],
+    // 2. 
+
 
 })
 
