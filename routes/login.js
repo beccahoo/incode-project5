f21a66c425816c8f64bd7356bd1cb75bcda7684f
Vei Yie <veiyie1998@gmail.com>
2021-09-21 17:44:17 +0800

    Updated 1.1 step in displaying error and rendering login ejs page which will be created by Bec

--- routes/login.js
+++ routes/login.js
@@ -1,13 +1,13 @@
 const express = require('express')
 const router = express.Router
 const bcrypt = require("bcryptjs")
-const db = require('../database')
-const { check, validationResult } = require('express-validator')
+const db = require('../database') // not yet created database file
+const { check, validationResult } = require('express-validator') // I downloaded express-validator to validate my email and password
 
 router.post('/', (req, res) => {
     const {email, password} = req.body
 
-    // 1. Validate email 
+    // 1. Validate email and password
     [ check( "email", "Please enter valid email")
     .isEmail()
     .normalizeEmail()
@@ -18,7 +18,22 @@ router.post('/', (req, res) => {
     .isLength({min: 8})
     .trim()
 ],
-    // 2. 
+    // 1.1 Insert validation, display error and render login page again
+    const {email, password} = req.body
+    const errors = validationResult()
+
+    if(!errors.isEmpty()) { //  make sure that an input on a form is not empty
+        const errMsgs = errors.array();
+        res.render("../view/pages/login", {errMsgs}) // have not created login ejs page
+    } else {
+
+        // 2. Check if the email exist in the database
+        
+
+    }
+
+
+
 
 
 })
