db9b35ceea677bc5c6ef345cfafd30815ed7473b
Vei Yie <veiyie1998@gmail.com>
2021-09-23 09:26:50 +0800

    Updated login route

--- routes/login.js
+++ routes/login.js
@@ -1,64 +1,60 @@
 const express = require('express')
-const router = express.Router
+const router = express.Router()
 const bcrypt = require("bcryptjs")
 const db = require('../database') // not yet created database file
 const { check, validationResult } = require('express-validator') // I downloaded express-validator to validate my email and password
-
+// router.get('/login', (req, res) => {
+//     res.render('./pages/login')
+// })
 // add redirecttoHome if can't logged in --> Home is the movie page
-router.post('/', (req, res) => {
-    const {email, password} = req.body
-
+router.post("/login",
     // 1. Validate email and password
-    [ check( "email", "Please enter valid email")
-    .isEmail()
-    .normalizeEmail()
-    .trim()
-    .toLowerCase(),
-
-    check ("password", "Please enter valid password")
-    .isLength({min: 8})
-    .trim()
-],
-    // 1.1 Insert validation, display error and render login page again
-    const {email, password} = req.body
-    const errors = validationResult()
-
-    if(!errors.isEmpty()) { //  make sure that an input on a form is not empty
-        const errMsgs = errors.array();
-        res.render("../view/pages/login", {errMsgs}) // have not created login ejs page
-    } else {
-
-        // 2. Check if the email exist in the database
-        const cleanedEmail = email.toLowercase().trim()
-        db.oneOrNone('SELECT * FROM users WHERE email=$1,', cleanedEmail)
-        .then (user => {
-            if(!user) {
-                res.redirect('/login?message=Email%20or%20password%20is%20invalid')
+    [check("email", "Please enter valid email")
+        .isEmail()
+        .normalizeEmail()
+        .trim()
+        .toLowerCase(),
+    check("password", "Please enter valid password")
+        .isLength({ min: 8 })
+        .trim(),
+    ],
+    async((req, res) => {
+        try {
+            const { email, password } = req.body;
+            const errors = validationResult(req);
+            if (!errors.isEmpty()) {
+                const errMsgs = errors.array();
+                res.render("../view/pages/login", { errMsgs });
             } else {
-
-                // 3. If so, verify password and edit session
-                bcrypt.compare(password, user.password)
-                .then(result => {
-                    if(result) {
-                        // 3.1 Edit session and redirect with success message
-                        req.session.userId = user.userId // check whether userId entered is equal to userID stored in db
-                        res.send('Successfully logged in!')
-                    
-                    } else {
-                        res.redirect('/login?message=Email%20or%20password%20is%20incorrect')
-                    }
-                })
-                .catch(error => {
-                    console.log(error)
-                    res.send(error)
-                })
+                //check if email exists in db
+                const user = await db.oneOrNone(
+                    "SELECT * FROM users WHERE email = $1;",
+                    email
+                );
+                if (!user) {
+                    res.redirect("/login?message=User%20does%20not%20exist.")
+                } else {
+                    bcrypt.compare(password, user.password, (err, result) => {
+                        try {
+                            if (result) {
+                                req.session.userId = user.user_id;
+                                req.session.userEmail = user.email;
+                                res.redirect("/");
+                            } else {
+                                res.redirect('/login?message=Email%20or%20password%20is%20incorrect.');
+                            }
+                        } catch (err) {
+                            console.log(err);
+                            res.send(err);
+                        }
+                    });
+                }
             }
-        })
-        .catch(error => {
-            console.log(error)
-            res.send(error)
-        })
-    }
-})
-
+        }
+        catch (err) {
+            console.log(err)
+        }
+    })
+);
+    // 1.1 Insert validation, display error and render login page again
 module.exports = router
