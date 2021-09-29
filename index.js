dcf0b4246e23641e4b0d6e9165d9d12bddb5465e
veiyie <veiyie1998@gmail.com>
2021-09-30 03:54:04 +0800

    Refined register js page

--- index.js
+++ index.js
@@ -14,12 +14,11 @@ const app = express();
 const genres =  [
     {"id":28,"name":"Action"},{"id":12,"name":"Adventure"},{"id":16,"name":"Animation"},{"id":35,"name":"Comedy"},{"id":80,"name":"Crime"},{"id":99,"name":"Documentary"},{"id":18,"name":"Drama"},{"id":10751,"name":"Family"},{"id":14,"name":"Fantasy"},{"id":36,"name":"History"},{"id":27,"name":"Horror"},{"id":10402,"name":"Music"},{"id":9648,"name":"Mystery"},{"id":10749,"name":"Romance"},{"id":878,"name":"Science Fiction"},{"id":10770,"name":"TV Movie"},{"id":53,"name":"Thriller"},{"id":10752,"name":"War"},{"id":37,"name":"Western"}
 ]
-
+// Genre movie genre tags 
 const tagsE1 = document.getElementById("tags");
 
 
-
-//setting view enginne and layouts
+//setting view engine and layouts
 app.set('view engine', 'ejs')
 app.set('views', 'views');
 app.use(express.static('public'));
@@ -77,6 +76,7 @@ function setGenre() {
 
 }
 
+// Add highlight selection function for users to know what genres have been selected
 function highlightSelection()  {
     document.querySelectorAll('.tag')
     tags.forEach(tag => {
@@ -88,6 +88,37 @@ function highlightSelection()  {
             hightlightedTag.classlist.add('highlight');
         })
     }
+}
+
+function clearBtn() {
+    let clearBtn = document.getElementById('clear');
+    if(clearBtn) {
+        clearBtn.classList.add('highlight')
+    } else {
+
+        let clear = document.createElement('div')
+        clear.classList.add('tag', 'highlight');
+        clear.id = 'clear';
+        clear.innerText = 'Clear x';
+        clear.addEventListener('click', () => {
+            selectedGenre = [];
+            setGenre();
+            getMovies(API_URL);
+        })
+        tagsE1.append(clear); 
+    }
+}
+
+getMovies(API_URL);
+function getMovies(url) {
+    fetch(url).then(res => res,json()).then(data => {
+        console.log(data.results)
+        if(data.results.length !== 0){
+        showMovies(data.results);
+        } else {
+            main.innerHTML = `<h1 class="no-results">No Results Found </h1>`
+        }
+    })
 }
 
 
